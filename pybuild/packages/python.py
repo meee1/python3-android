c69e61fff212a537db791e57f71b6f0b469625b3
Yen Chi Hsuan <yan12125@gmail.com>
2017-09-10 18:20:37 +0800

    binutils-2.27 test
    * Remove NDK issue 399 patch
    * Remove -fno-integrated-as for arm64, which was breaking the build with
      new binutils
    
    pybuild-rebuild=ALL

--- pybuild/packages/python.py
+++ pybuild/packages/python.py
@@ -14,7 +14,6 @@ class Python(Package):
         LocalPatch('nl_langinfo'),
         LocalPatch('cppflags'),
         LocalPatch('skip-build'),
-        LocalPatch('ndk-issue399'),
     ]
 
     def __init__(self):
